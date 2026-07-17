9f8eb55a041182bdda0136e2af361a4ed549bcdb
Mayur-patil-5 <mayurpatil.work20@gmail.com>
2026-07-17 20:07:20 +0530

    Sync LeetCode solution: GCD of Odd and Even Sums (Java)

---
+++ Easy/3995_GCD_of_Odd_and_Even_Sums.java
@@ -0,0 +1,16 @@
+/*
+ * Problem: GCD of Odd and Even Sums
+ * Problem ID: 3995
+ * Difficulty: Easy
+ * Language: Java
+ * Runtime: 1 ms
+ * Memory: 42.3 MB
+ * Synced From: LeetCode
+ * Date: 2026-07-17
+ */
+
+class Solution {
+    public int gcdOfOddEvenSums(int n) {
+        return n;
+    }
+}
