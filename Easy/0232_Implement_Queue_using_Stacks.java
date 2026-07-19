6625c3b879a7af1bbdf5a3aba6b63cf0ea82a23a
Mayur-patil-5 <mayurpatil.work20@gmail.com>
2026-07-19 14:53:24 +0530

    Sync LeetCode solution: Implement Queue using Stacks (Java)

---
+++ Easy/0232_Implement_Queue_using_Stacks.java
@@ -0,0 +1,53 @@
+/*
+ * Problem: Implement Queue using Stacks
+ * Problem ID: 232
+ * Difficulty: Easy
+ * Language: Java
+ * Runtime: 2 ms
+ * Memory: 42.8 MB
+ * Synced From: LeetCode
+ * Date: 2026-07-19
+ */
+
+import java.util.Stack;
+
+class MyQueue {
+
+    Stack<Integer> s1;
+    Stack<Integer> s2;
+
+    public MyQueue() {
+        s1 = new Stack<>();
+        s2 = new Stack<>();
+    }
+
+    public void push(int x) {
+        while (!s1.isEmpty()) {
+            s2.push(s1.pop());
+        }
+
+        s1.push(x);
+
+        while (!s2.isEmpty()) {
+            s1.push(s2.pop());
+        }
+    }
+
+    public int pop() {
+        if (s1.isEmpty()) {
+            return -1;
+        }
+        return s1.pop();
+    }
+
+    public int peek() {
+        if (s1.isEmpty()) {
+            return -1;
+        }
+        return s1.peek();
+    }
+
+    public boolean empty() {
+        return s1.isEmpty();
+    }
+}
