0e0ea0644ee860c0e49aaa61af239152d77f8ff8
Mayur-patil-5 <mayurpatil.work20@gmail.com>
2026-08-01 23:25:17 +0530

    Sync LeetCode solution: Two Sum (Java)

--- Easy/0001_Two_Sum.java
+++ Easy/0001_Two_Sum.java
@@ -4,29 +4,20 @@
  * Difficulty: Easy
  * Language: Java
  * Runtime: 45 ms
- * Memory: 46.9 MB
+ * Memory: 46.8 MB
  * Synced From: LeetCode
  * Date: 2026-08-01
  */
 
 public class Solution {
     public static int[] twoSum(int[] nums, int target) {
-        int res[]=new int[2];
-         for(int i=0;i<nums.length;i++){
-            for(int j=i+1;j<nums.length;j++){
-            if(nums[i]+nums[j]==target){
-                 res[0]=i;
-                 res[1]=j;
-                 return res;
+    for (int i = 0; i < nums.length; i++) {
+        for (int j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] == target) {
+                return new int[] { i, j };
             }
         }
-        }
-        return res;
     }
-    public static void main(String[] args) {
-        int nums[]={2,7,11,15};
-        int target=9;
-        int ans[]=twoSum(nums, target);
-        System.out.println(ans[0]+" "+ans[1]);
-    }
+    return new int[] {};
+}
 }
