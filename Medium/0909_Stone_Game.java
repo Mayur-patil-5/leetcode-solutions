11ff375482357b00788492d42048fa80a46045c2
Mayur-patil-5 <mayurpatil.work20@gmail.com>
2026-08-02 20:46:41 +0530

    Sync LeetCode solution: Stone Game (Java)

---
+++ Medium/0909_Stone_Game.java
@@ -0,0 +1,16 @@
+/*
+ * Problem: Stone Game
+ * Problem ID: 909
+ * Difficulty: Medium
+ * Language: Java
+ * Runtime: 0 ms
+ * Memory: 43.1 MB
+ * Synced From: LeetCode
+ * Date: 2026-08-02
+ */
+
+class Solution {
+    public boolean stoneGame(int[] piles) {
+        return true;
+    }
+}
