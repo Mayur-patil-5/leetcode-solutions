967f9746182b39a52f50b436faf0f3820c032558
Mayur-patil-5 <mayurpatil.work20@gmail.com>
2026-08-19 00:22:13 +0530

    Sync LeetCode solution: Find the Largest Almost Missing Integer (Java)

---
+++ Easy/3705_Find_the_Largest_Almost_Missing_Integer.java
@@ -0,0 +1,40 @@
+/*
+ * Problem: Find the Largest Almost Missing Integer
+ * Problem ID: 3705
+ * Difficulty: Easy
+ * Language: Java
+ * Runtime: 5 ms
+ * Memory: 46.7 MB
+ * Synced From: LeetCode
+ * Date: 2026-08-18
+ */
+
+class Solution {
+    public int largestInteger(int[] nums, int k) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i <= nums.length - k; i++) {
+
+            HashSet<Integer> set = new HashSet<>();
+
+            for (int j = i; j < i + k; j++) {
+                set.add(nums[j]);
+            }
+
+            for (int num : set) {
+                map.put(num, map.getOrDefault(num, 0) + 1);
+            }
+        }
+
+        int ans = -1;
+
+        for (int num : map.keySet()) {
+            if (map.get(num) == 1) {
+                ans = Math.max(ans, num);
+            }
+        }
+
+        return ans;
+    }
+}
