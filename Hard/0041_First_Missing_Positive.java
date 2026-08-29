6f2320f0ed91addce01af9293bd3a315771d8efe
Mayur-patil-5 <mayurpatil.work20@gmail.com>
2026-08-29 19:12:14 +0530

    Sync LeetCode solution: First Missing Positive (Java)

---
+++ Hard/0041_First_Missing_Positive.java
@@ -0,0 +1,34 @@
+/*
+ * Problem: First Missing Positive
+ * Problem ID: 41
+ * Difficulty: Hard
+ * Language: Java
+ * Runtime: 1 ms
+ * Memory: 71.1 MB
+ * Synced From: LeetCode
+ * Date: 2026-08-29
+ */
+
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        int n=nums.length;
+        int i=0;
+        while(i<n){
+            int correctidx=nums[i]-1;
+            if(nums[i]>0 && nums[i]<=n && nums[i]!=nums[correctidx]){
+                int temp=nums[i];
+                nums[i]=nums[correctidx];
+                nums[correctidx]=temp;
+            }
+            else{
+                i++;
+            }
+        }
+        for(i=0;i<n;i++){
+            if(nums[i]!=i+1){
+                return i+1;
+            }
+        }
+        return n+1;
+    }
+}
