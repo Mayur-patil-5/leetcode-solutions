a00c12e511047fd97f0a6dfe98f68620bc81c34e
Mayur-patil-5 <mayurpatil.work20@gmail.com>
2026-09-01 23:26:31 +0530

    Sync LeetCode solution: Count Primes (Java)

---
+++ Medium/0204_Count_Primes.java
@@ -0,0 +1,37 @@
+/*
+ * Problem: Count Primes
+ * Problem ID: 204
+ * Difficulty: Medium
+ * Language: Java
+ * Runtime: 792 ms
+ * Memory: 79.8 MB
+ * Synced From: LeetCode
+ * Date: 2026-09-01
+ */
+
+//Note-Sieve of Eratosthenes approach
+//2 to n-1 tk numbers track kro if i=2 tho 2 ke sb multiples remove kr do 
+//next i=3 3 ke sb multiple remove and then jo bacha wo prime number.
+
+class Solution {
+    public int countPrimes(int n) {
+        boolean[] isPrime = new boolean[n];
+        for (int i = 2; i < n; i++) {
+            isPrime[i] = true;
+        }
+        for (int i = 2; i * i < n; i++) {
+            if (isPrime[i]) {
+                for (int j = i * i; j < n; j += i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+        int count = 0;
+        for (int i = 2; i < n; i++) {
+            if (isPrime[i]) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
