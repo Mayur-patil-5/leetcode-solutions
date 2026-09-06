f870e7ed577c6ea3f9c19e1c70b860972d7cbcf0
Mayur-patil-5 <mayurpatil.work20@gmail.com>
2026-09-06 18:16:51 +0530

    Sync LeetCode solution: To Lower Case (Java)

---
+++ Easy/0742_To_Lower_Case.java
@@ -0,0 +1,17 @@
+/*
+ * Problem: To Lower Case
+ * Problem ID: 742
+ * Difficulty: Easy
+ * Language: Java
+ * Runtime: 0 ms
+ * Memory: 42.3 MB
+ * Synced From: LeetCode
+ * Date: 2026-09-06
+ */
+
+class Solution {
+    public String toLowerCase(String s) {
+        String newstr=s.toLowerCase();
+        return newstr;
+    }
+}
