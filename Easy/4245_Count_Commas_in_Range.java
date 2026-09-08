1b2f41551fc433c370d3bb2bcbed8380a2555786
Mayur-patil-5 <mayurpatil.work20@gmail.com>
2026-09-08 23:27:12 +0530

    Sync LeetCode solution: Count Commas in Range (Java)

--- Easy/4245_Count_Commas_in_Range.java
+++ Easy/4245_Count_Commas_in_Range.java
@@ -3,19 +3,19 @@
  * Problem ID: 4245
  * Difficulty: Easy
  * Language: Java
- * Runtime: 6 ms
- * Memory: 42.4 MB
+ * Runtime: 1 ms
+ * Memory: 42.5 MB
  * Synced From: LeetCode
  * Date: 2026-09-08
  */
 
 class Solution {
     public int countCommas(int n) {
-        // return Math.max(n-999,0);  
-        int count=0;
-        for(int i=1000;i<=n;i++){
-            count++;
-        }
-        return count;
+        return Math.max(n-999,0);  
+        // int count=0;
+        // for(int i=1000;i<=n;i++){
+        //     count++;
+        // }
+        // return count;
     }
 }
