b7f11134b3fd2e4b1761fa1dc2924b5b3d6ee932
Mayur-patil-5 <mayurpatil.work20@gmail.com>
2026-09-11 19:06:48 +0530

    Sync LeetCode solution: Pow(x, n) (Java)

---
+++ Medium/0050_Powx_n.java
@@ -0,0 +1,16 @@
+/*
+ * Problem: Pow(x, n)
+ * Problem ID: 50
+ * Difficulty: Medium
+ * Language: Java
+ * Runtime: 0 ms
+ * Memory: 48.1 MB
+ * Synced From: LeetCode
+ * Date: 2026-09-11
+ */
+
+class Solution {
+    public double myPow(double x, int n) {
+        return Math.pow(x,n);
+    }
+}
